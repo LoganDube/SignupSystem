f879920d32343fbca6f7bd5a4251e1816dc05c8b
Logan Dube <108110047+vScoped@users.noreply.github.com>
2023-06-16 17:51:41 +1000

    Delete website directory

--- website/index.py
+++
@@ -1,7 +0,0 @@
-from main import create_app
-
-app = create_app()
-
-#running of the app
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
--- website/static/delete_button.js
+++
@@ -1,9 +0,0 @@
-//delete note from database and user interface
-function deleteNote(noteId) {
-  fetch("/delete-note", {
-    method: "POST",
-    body: JSON.stringify({ noteId: noteId }),
-  }).then((_res) => {
-    window.location.href = "";
-  });
-}
